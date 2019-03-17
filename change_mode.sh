1bcdfe807cdbe99992de5b48ec305f779dcc16d6
K1-scuba_diving <tellurium38@gmail.com>
2019-03-18 03:23:34 +0900

    add start.sh comment and condition

---
+++ change_mode.sh
@@ -0,0 +1,14 @@
+# !/bin/bash
+
+#cnt=$(find /home/guralin/.cache/ahifaoihjifoea -name 'dein' | wc -l)
+#echo ${cnt}
+#find /home/guralin/.cache/ -name 'dein' | wc -l
+
+#files=$(ls)
+#echo ${files}
+
+if [ -e /home/guralin/.cache/dein ]; then
+    echo "すごいね"
+else
+    echo "あんまりすごくないね"
+fi
--- start.sh
+++ start.sh
@@ -7,7 +7,19 @@ ln -sf ~/dotfiles/colors ~/.vim
 ln -sf ~/dotfiles/templates ~/.vim
 ln -sf ~/dotfiles/after ~/.vim
 
+cat << EOS
+~/dotfiles/.vimrc 
+のシンボリックリンクを~/.vimrcに置き換え
+~/dotfiles/colors
+~/dotfiles/templates
+~/dotfiles/after 
+のシンボリックリンクを~/.vimに配置
+EOS
 
-# dein.vimをインストール
+if [ -e /home/guralin/.cache/dein ]; then
+    echo "deinはインストール済み"
+else
+    sh bash/installer.sh ~/.cache/dein
+fi
 
-sh bash/installer.sh ~/.cache/dein
+echo "Done!!"
