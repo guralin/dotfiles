3af86d1c4fda9ac9fd5ddd7d9245000edc7425d8
K1-scuba_diving <tellurium38@gmail.com>
2019-07-14 15:09:27 +0900

    add_template_for_atcoder

---
+++ templates/Python_atcoder.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from sys import stdin
+import math
+
+# 標準入力をスペースで区切って、intに変換 listに収納
+int_list  = [int(x) for x in stdin.readline().rstrip().split()]
